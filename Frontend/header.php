396bd422197133c0aed677358c9c35889b52ccd8
Mammew <mammolo103@gmail.com>
2024-12-26 18:41:44 +0100

    Creatind css folder. Changing the link for css due to the creation of other file for having a better modularity

--- Frontend/header.php
+++ Frontend/header.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SeaFlavor</title>
     <link rel="icon" type="image/x-icon" href="images/image.png">
-    <link rel="stylesheet" type="text/css" href="home.css"> <!-- Da cambiare il foglio di stile prob.. -->
+    <link rel="stylesheet" type="text/css" href="css/home.css"> <!-- Da cambiare il foglio di stile prob.. -->
+    <link rel="stylesheet" type="text/css" href="css/sticky_header.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
 </head>
 <body>
     <header>
--- Frontend/home.php
+++ Frontend/home.php
@@ -23,35 +23,28 @@
 
     <section class="services">
         <div class="service">
-            <h1>ciaooo</h1>
+            <h1>Noi</h1>
             <p>
-                fdavdfjvhgagvjhadfvjhbfjhv
-                vfdjvhjdvjhdfvhjbdjbvjdfav
-                afvjdfjvbjhbvjhfafv
-                vajbvjhfbhvbdfkvbdf
-                fvhjdfhvlfdffvj
+                In SeaFlavour, la nostra passione è il Mar Ligure e la sua ricchezza.<br>
+                Ci impegniamo a promuovere una pesca sostenibile, che preservi l'ecosistema marino e offra al contempo pesce fresco e di alta qualità. 
+                Portiamo il meglio del pescato ligure direttamente nelle vostre case, condividendo la storia e la tradizione di un territorio unico.
             </p>
         </div>
 
         <div class="service">
-            <h1>mondo</h1>
+            <h1>Prodotti</h1>
             <p>
-                fdavdfjvhgagvjhadfvjhbfjhv
-                vfdjvhjdvjhdfvhjbdjbvjdfav
-                afvjdfjvbjhbvjhfafv
-                vajbvjhfbhvbdfkvbdf
-                fvhjdfhvlfdffvj
+                Ogni giorno, i pescatori liguri solcano il mare per portare a casa un tesoro di freschezza.<br>
+                Da noi trovi il loro pescato migliore, frutto di una pesca sostenibile che rispetta i ritmi della natura. 
+                E per chi ama il mare, offriamo prodotti per una pesca più responsabile.
             </p>
         </div>
 
         <div class="service">
-            <h1>gay</h1>
+            <h1>Cucina</h1>
             <p>
-                fdavdfjvhgagvjhadfvjhbfjhv
-                vfdjvhjdvjhdfvhjbdjbvjdfav
-                afvjdfjvbjhbvjhfafv
-                vajbvjhfbhvbdfkvbdf
-                fvhjdfhvlfdffvj
+                Esplora un viaggio culinario tra le ricette di pesce della Liguria, 
+                dalle preparazioni più semplici e gustose come il pesce al forno alla ligure a creazioni più elaborate che sapranno stupire i tuoi ospiti.
             </p>
         </div>
     </section>
