7c4e99c2b846f190aa475b6a785b1e9537dd31ff
Mammew <mammolo103@gmail.com>
2025-01-11 22:14:57 +0100

    fixed registration with an already used email

--- Frontend/js/registration.js
+++ Frontend/js/registration.js
@@ -1,5 +1,7 @@
 let email_error = document.getElementById("email_error");
 let mail = document.getElementById("email");
+let form = document.getElementById("form");
+let emailUsed = false;
 mail.addEventListener("change",function checkmail(){
     let usermail = mail.value;
     let url = "../Backend/checkMail.php";
@@ -18,8 +20,21 @@ mail.addEventListener("change",function checkmail(){
     .then(text => {
         if (text == "0") {
             email_error.innerHTML = "";
+            emailUsed = false;
         }
-        else if(text == "1")
-            email_error.innerHTML = "email già in uso, inserirne un'altra";
+        else if(text == "1"){
+            email_error.innerHTML = "email giÃ in uso, inserirne un'altra";
+            emailUsed = true;
+        }
         })
+    })
+
+    form.addEventListener("submit", function(event){
+        if (emailUsed) {
+            event.preventDefault();
+            console.log("invio bloccato");
+        }
+        else{
+            console.log("invio permesso");
+        }
     })
